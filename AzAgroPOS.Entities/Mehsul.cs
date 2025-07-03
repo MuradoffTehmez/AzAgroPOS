e8dae42b7e2474b2bd95e669a0f4fe71b3c80b12
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-04 02:50:15 +0400

    Add Mehsul and Vahid classes to Entities namespace
    Introduced two new classes, `Mehsul` and `Vahid`, in the `AzAgroPOS.Entities` namespace. The `Mehsul` class includes various properties for product details, while the `Vahid` class contains properties for unit information. These additions enhance the data structure for the AzAgroPOS application.

---
+++ AzAgroPOS.Entities/Mehsul.cs
@@ -0,0 +1,27 @@
+﻿// Fayl: AzAgroPOS.Entities/Mehsul.cs
+
+using System;
+
+namespace AzAgroPOS.Entities
+{
+    public class Mehsul
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Barkod { get; set; }
+        public int KateqoriyaId { get; set; }
+        public int VahidId { get; set; }
+        public decimal AlisQiymeti { get; set; }
+        public decimal SatisQiymeti { get; set; }
+        public int MinimumStok { get; set; }
+        public int CariStok { get; set; }
+        public int? TedarukcuId { get; set; } // Nullable ola bilər
+        public string Tesvir { get; set; }
+        public bool Aktivdir { get; set; }
+        public bool Silinib { get; set; }
+
+        // Bu xüsusiyyətlər View-dan gələcək, birbaşa cədvəldə yoxdur
+        public string KateqoriyaAdi { get; set; }
+        public string VahidAdi { get; set; }
+    }
+}
---
+++ AzAgroPOS.Entities/Vahid.cs
@@ -0,0 +1,11 @@
+﻿// Fayl: AzAgroPOS.Entities/Vahid.cs
+
+namespace AzAgroPOS.Entities
+{
+    public class Vahid
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Qisaltma { get; set; }
+    }
+}
