3e48dbc7261f53c99e475637314d0407f8c682a3
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-04 03:04:22 +0400

    Add KateqoriyaBLL class for category management
    Introduces the `KateqoriyaBLL` class in the `AzAgroPOS.BLL` namespace.
    This class includes using directives for necessary namespaces and
    contains a private instance of `KateqoriyaDAL`. It provides a
    method `GetAll()` to retrieve a list of `Kateqoriya` objects.

---
+++ AzAgroPOS.BLL/KateqoriyaBLL.cs
@@ -0,0 +1,12 @@
+﻿using AzAgroPOS.DAL;
+using AzAgroPOS.Entities;
+using System.Collections.Generic;
+
+namespace AzAgroPOS.BLL
+{
+    public class KateqoriyaBLL
+    {
+        private readonly KateqoriyaDAL _dal = new KateqoriyaDAL();
+        public List<Kateqoriya> GetAll() => _dal.GetAll();
+    }
+}
