2b106225448e9e1512f54f55e803586534eaee71
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-04 02:49:52 +0400

    Fix file header comment and simplify login process comments
    Updated the file header from "Fayl" to "Düzgün FAYL" for clarity. Removed several comments related to the login process to streamline the code and reduce unnecessary explanations about the application's flow.

--- AzAgroPOS.PL/Program.cs
+++ AzAgroPOS.PL/Program.cs
@@ -1,5 +1,4 @@
-﻿// Fayl: AzAgroPOS.PL/Program.cs
-
+﻿// Düzgün FAYL: AzAgroPOS.PL/Program.cs
 using AzAgroPOS.Entities;
 using System;
 using System.Windows.Forms;
@@ -8,27 +7,17 @@ namespace AzAgroPOS.PL
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // Login formasını yarat və dialoq kimi göstər.
-            // Kod bu sətirdə login forması bağlanana qədər gözləyəcək.
             frmLogin loginForm = new frmLogin();
             if (loginForm.ShowDialog() == DialogResult.OK)
             {
-                // Əgər login uğurlu olubsa (DialogResult.OK qayıdıbsa),
-                // o zaman ana pəncərəni işə salırıq.
-                // Daxil olan istifadəçi məlumatını login formasından alırıq.
                 Application.Run(new frmMain(loginForm.LoggedInUser));
             }
-            // Əgər login uğursuz olarsa (məsələn, Ləğv Et və ya X basılarsa),
-            // if bloku işləməyəcək və Main metodu bitdiyi üçün proqram bağlanacaq.
         }
     }
 }
