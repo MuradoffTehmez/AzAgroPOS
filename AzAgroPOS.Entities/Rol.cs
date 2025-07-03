bbb32ed862e33e0d54f6d072170b4b8a89fe2266
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-03 21:58:29 +0400

    Remove Class1 and add Rol class in Entities project
    Updated AzAgroPOS.BLL, AzAgroPOS.DAL, and AzAgroPOS.Entities project files to remove references to `Class1.cs` and `Properties\AssemblyInfo.cs`. Added new `Rol.cs` file with an internal class `Rol`. Reformatted `ProjectGuid` casing in Entities project and cleaned up `using` directives in deprecated Class1 files. Reformatted references for consistency in AzAgroPOS.Entities.csproj.

--- AzAgroPOS.Entities/Rol.cs
+++ AzAgroPOS.Entities/Rol.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AzAgroPOS.DAL
+namespace AzAgroPOS.Entities
 {
-    public class Class1
+    internal class Rol
     {
     }
 }
