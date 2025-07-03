010ec85f77f5a2e6a5de32ee9ef4a128b05c50dc
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-04 02:49:42 +0400

    Add VahidDAL class for database access
    Introduces the `VahidDAL` class in the `AzAgroPOS.DAL` namespace. This class manages database connections and includes a method `GetAll` to retrieve `Vahid` objects from the `vahidler` table. Implements basic error handling for database operations.

---
+++ AzAgroPOS.DAL/VahidDAL.cs
@@ -0,0 +1,49 @@
+﻿using AzAgroPOS.DAL.Helpers;
+using AzAgroPOS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace AzAgroPOS.DAL
+{
+    public class VahidDAL
+    {
+        private readonly string _connectionString;
+
+        public VahidDAL()
+        {
+            _connectionString = DatabaseHelper.GetConnectionString();
+        }
+
+        public List<Vahid> GetAll()
+        {
+            var vahidler = new List<Vahid>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var query = "SELECT id, ad, qisaltma FROM vahidler ORDER BY ad;";
+                var command = new SqlCommand(query, connection);
+                try
+                {
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            vahidler.Add(new Vahid
+                            {
+                                Id = (int)reader["id"],
+                                Ad = reader["ad"].ToString(),
+                                Qisaltma = reader["qisaltma"].ToString()
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+            return vahidler;
+        }
+    }
+}
