df6935bff56a99c2056c882b46eb5fca4c7df27e
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-03 20:02:54 +0400

    Add AzAgroPOS.BLL project and initial structure
    This commit introduces a new project "AzAgroPOS.BLL" to the solution, including its project file (`AzAgroPOS.BLL.csproj`) with configurations for Debug and Release modes, and references to essential .NET libraries.
    
    A new class file `Class1.cs` is added with a basic class definition, and an `AssemblyInfo.cs` file is created to provide assembly metadata.
    
    These changes establish the business logic layer (BLL) for the AzAgroPOS application, laying the groundwork for future development.

---
+++ AzAgroPOS.BLL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzAgroPOS.BLL
+{
+    public class Class1
+    {
+    }
+}
