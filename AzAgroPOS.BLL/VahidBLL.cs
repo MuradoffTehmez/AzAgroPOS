8a7c6cad75a47d7ac6d35e2127b5c56bfc4b67f6
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-04 03:04:47 +0400

    Add VahidBLL class for data access operations
    Introduces the `VahidBLL` class in the `AzAgroPOS.BLL` namespace.
    Includes using directives for necessary namespaces and
    instantiates a private `VahidDAL` field for data access.
    Adds a public method `GetAll` to retrieve a list of
    `Vahid` objects from the data layer.

---
+++ AzAgroPOS.BLL/VahidBLL.cs
@@ -0,0 +1,12 @@
+﻿using AzAgroPOS.DAL;
+using AzAgroPOS.Entities;
+using System.Collections.Generic;
+
+namespace AzAgroPOS.BLL
+{
+    public class VahidBLL
+    {
+        private readonly VahidDAL _dal = new VahidDAL();
+        public List<Vahid> GetAll() => _dal.GetAll();
+    }
+}
