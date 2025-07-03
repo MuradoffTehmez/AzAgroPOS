f50ac778916a1692fa1cae8ac93ba269f6d626ec
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-04 02:49:29 +0400

    Add KateqoriyaDAL class for category data access
    Introduces the `KateqoriyaDAL` class in the `AzAgroPOS.DAL` namespace. This class provides a method to retrieve active categories from the database. It includes a constructor for initializing the connection string and implements exception handling for database operations.

---
+++ AzAgroPOS.DAL/KateqoriyaDAL.cs
@@ -0,0 +1,48 @@
+﻿using AzAgroPOS.DAL.Helpers;
+using AzAgroPOS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace AzAgroPOS.DAL
+{
+    public class KateqoriyaDAL
+    {
+        private readonly string _connectionString;
+
+        public KateqoriyaDAL()
+        {
+            _connectionString = DatabaseHelper.GetConnectionString();
+        }
+
+        public List<Kateqoriya> GetAll()
+        {
+            var kateqoriyalar = new List<Kateqoriya>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var query = "SELECT id, ad FROM kateqoriyalar WHERE aktivdir = 1 ORDER BY ad;";
+                var command = new SqlCommand(query, connection);
+                try
+                {
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            kateqoriyalar.Add(new Kateqoriya
+                            {
+                                Id = (int)reader["id"],
+                                Ad = reader["ad"].ToString()
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+            return kateqoriyalar;
+        }
+    }
+}
