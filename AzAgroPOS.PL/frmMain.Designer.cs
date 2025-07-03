98aa09e9fed742e155c6ff86b0b1bc5714336b13
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-04 02:23:17 +0400

    Add frmMain class with menu and status strip
    Introduced a new Windows Forms application class `frmMain` in the `AzAgroPOS.PL` namespace. Updated the designer file to include a menu strip with items for File, Exit, Sales, Warehouse, Repair, Customers, Reports, and Settings. Added a status strip to display the current user's name. The constructor accepts a logged-in user of type `Istifadeci`, and the `frmMain_Load` method updates the status strip accordingly. Implemented an event handler for the exit menu item to close the application.

---
+++ AzAgroPOS.PL/frmMain.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace AzAgroPOS.PL
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.faylToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.çıxışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.satışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.anbarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.təmirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.müştərilərToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hesabatlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tənzimləmələrToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblCurrentUser = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.faylToolStripMenuItem,
+            this.satışToolStripMenuItem,
+            this.anbarToolStripMenuItem,
+            this.təmirToolStripMenuItem,
+            this.müştərilərToolStripMenuItem,
+            this.hesabatlarToolStripMenuItem,
+            this.tənzimləmələrToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(754, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // faylToolStripMenuItem
+            // 
+            this.faylToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.çıxışToolStripMenuItem});
+            this.faylToolStripMenuItem.Name = "faylToolStripMenuItem";
+            this.faylToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.faylToolStripMenuItem.Text = "Fayl";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // çıxışToolStripMenuItem
+            // 
+            this.çıxışToolStripMenuItem.Name = "çıxışToolStripMenuItem";
+            this.çıxışToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.çıxışToolStripMenuItem.Text = "Çıxış";
+            this.çıxışToolStripMenuItem.Click += new System.EventHandler(this.çıxışToolStripMenuItem_Click);
+            // 
+            // satışToolStripMenuItem
+            // 
+            this.satışToolStripMenuItem.Name = "satışToolStripMenuItem";
+            this.satışToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.satışToolStripMenuItem.Text = "Satış";
+            // 
+            // anbarToolStripMenuItem
+            // 
+            this.anbarToolStripMenuItem.Name = "anbarToolStripMenuItem";
+            this.anbarToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.anbarToolStripMenuItem.Text = "Anbar";
+            // 
+            // təmirToolStripMenuItem
+            // 
+            this.təmirToolStripMenuItem.Name = "təmirToolStripMenuItem";
+            this.təmirToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.təmirToolStripMenuItem.Text = "Təmir";
+            // 
+            // müştərilərToolStripMenuItem
+            // 
+            this.müştərilərToolStripMenuItem.Name = "müştərilərToolStripMenuItem";
+            this.müştərilərToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.müştərilərToolStripMenuItem.Text = "Müştərilər";
+            // 
+            // hesabatlarToolStripMenuItem
+            // 
+            this.hesabatlarToolStripMenuItem.Name = "hesabatlarToolStripMenuItem";
+            this.hesabatlarToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.hesabatlarToolStripMenuItem.Text = "Hesabatlar";
+            // 
+            // tənzimləmələrToolStripMenuItem
+            // 
+            this.tənzimləmələrToolStripMenuItem.Name = "tənzimləmələrToolStripMenuItem";
+            this.tənzimləmələrToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.tənzimləmələrToolStripMenuItem.Text = "Tənzimləmələr";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblCurrentUser});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 565);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(754, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblCurrentUser
+            // 
+            this.lblCurrentUser.Name = "lblCurrentUser";
+            this.lblCurrentUser.Size = new System.Drawing.Size(66, 17);
+            this.lblCurrentUser.Text = "Hazırlanır...";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 587);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AzAgroPOS - Ana Pəncərə";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem faylToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıxışToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem satışToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem anbarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem təmirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem müştərilərToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hesabatlarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tənzimləmələrToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel lblCurrentUser;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
---
+++ AzAgroPOS.PL/frmMain.cs
@@ -0,0 +1,39 @@
+﻿// Fayl: AzAgroPOS.PL/frmMain.cs
+
+using AzAgroPOS.Entities;
+using System;
+using System.Windows.Forms;
+
+namespace AzAgroPOS.PL
+{
+    public partial class frmMain : Form
+    {
+        // Daxil olan istifadəçini yadda saxlamaq üçün private sahə.
+        private readonly Istifadeci _currentUser;
+
+        // Constructor daxil olan istifadəçini qəbul edir.
+        public frmMain(Istifadeci loggedInUser)
+        {
+            InitializeComponent();
+            _currentUser = loggedInUser;
+        }
+
+        // Forma yüklənərkən bu hadisə işə düşür.
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // Status zolağındakı mətni yeniləyirik.
+            // Dizayn tərəfində StatusStrip-ə əlavə etdiyiniz label-in adı "lblCurrentUser" olmalıdır.
+            if (lblCurrentUser != null && _currentUser != null)
+            {
+                lblCurrentUser.Text = $"İstifadəçi: {_currentUser.Ad} {_currentUser.Soyad}";
+            }
+        }
+
+        // Fayl -> Çıxış menyusuna kliklədikdə proqramı bağlayırıq.
+        // Bunu etmək üçün frmMain dizayn pəncərəsində Fayl->Çıxış menyusuna iki dəfə klikləyib bu kodu yazın.
+        private void çıxışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
