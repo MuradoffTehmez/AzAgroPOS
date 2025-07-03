20caf36f4c76d0eabffe2571ae3bd88ea2c6b1f1
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-04 03:36:05 +0400

    add, elave olundu

---
+++ AzAgroPOS.PL/frmProducts.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace AzAgroPOS.PL
+{
+    partial class frmProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.gbProductDetails = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.cmbVahid = new System.Windows.Forms.ComboBox();
+            this.cmbKateqoriya = new System.Windows.Forms.ComboBox();
+            this.txtMinimumStok = new System.Windows.Forms.TextBox();
+            this.txtSatisQiymeti = new System.Windows.Forms.TextBox();
+            this.txtAlisQiymeti = new System.Windows.Forms.TextBox();
+            this.txtBarkod = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.gbProductDetails.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.AllowUserToAddRows = false;
+            this.dgvProducts.AllowUserToDeleteRows = false;
+            this.dgvProducts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvProducts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(12, 12);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.ReadOnly = true;
+            this.dgvProducts.RowHeadersWidth = 51;
+            this.dgvProducts.RowTemplate.Height = 24;
+            this.dgvProducts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProducts.Size = new System.Drawing.Size(818, 579);
+            this.dgvProducts.TabIndex = 0;
+            // 
+            // gbProductDetails
+            // 
+            this.gbProductDetails.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbProductDetails.Controls.Add(this.btnClear);
+            this.gbProductDetails.Controls.Add(this.btnDelete);
+            this.gbProductDetails.Controls.Add(this.btnUpdate);
+            this.gbProductDetails.Controls.Add(this.btnAdd);
+            this.gbProductDetails.Controls.Add(this.cmbVahid);
+            this.gbProductDetails.Controls.Add(this.cmbKateqoriya);
+            this.gbProductDetails.Controls.Add(this.txtMinimumStok);
+            this.gbProductDetails.Controls.Add(this.txtSatisQiymeti);
+            this.gbProductDetails.Controls.Add(this.txtAlisQiymeti);
+            this.gbProductDetails.Controls.Add(this.txtBarkod);
+            this.gbProductDetails.Controls.Add(this.txtAd);
+            this.gbProductDetails.Controls.Add(this.label7);
+            this.gbProductDetails.Controls.Add(this.label6);
+            this.gbProductDetails.Controls.Add(this.label5);
+            this.gbProductDetails.Controls.Add(this.label4);
+            this.gbProductDetails.Controls.Add(this.label3);
+            this.gbProductDetails.Controls.Add(this.label2);
+            this.gbProductDetails.Controls.Add(this.label1);
+            this.gbProductDetails.Location = new System.Drawing.Point(836, 12);
+            this.gbProductDetails.Name = "gbProductDetails";
+            this.gbProductDetails.Size = new System.Drawing.Size(336, 579);
+            this.gbProductDetails.TabIndex = 1;
+            this.gbProductDetails.TabStop = false;
+            this.gbProductDetails.Text = "Məhsul Məlumatları";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(19, 360);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(301, 35);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Təmizlə";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(232, 319);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(88, 35);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(125, 319);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(88, 35);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Yenilə";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(19, 319);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(88, 35);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Əlavə Et";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // cmbVahid
+            // 
+            this.cmbVahid.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbVahid.FormattingEnabled = true;
+            this.cmbVahid.Location = new System.Drawing.Point(135, 137);
+            this.cmbVahid.Name = "cmbVahid";
+            this.cmbVahid.Size = new System.Drawing.Size(185, 24);
+            this.cmbVahid.TabIndex = 4;
+            // 
+            // cmbKateqoriya
+            // 
+            this.cmbKateqoriya.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbKateqoriya.FormattingEnabled = true;
+            this.cmbKateqoriya.Location = new System.Drawing.Point(135, 102);
+            this.cmbKateqoriya.Name = "cmbKateqoriya";
+            this.cmbKateqoriya.Size = new System.Drawing.Size(185, 24);
+            this.cmbKateqoriya.TabIndex = 3;
+            // 
+            // txtMinimumStok
+            // 
+            this.txtMinimumStok.Location = new System.Drawing.Point(135, 242);
+            this.txtMinimumStok.Name = "txtMinimumStok";
+            this.txtMinimumStok.Size = new System.Drawing.Size(185, 22);
+            this.txtMinimumStok.TabIndex = 6;
+            // 
+            // txtSatisQiymeti
+            // 
+            this.txtSatisQiymeti.Location = new System.Drawing.Point(135, 207);
+            this.txtSatisQiymeti.Name = "txtSatisQiymeti";
+            this.txtSatisQiymeti.Size = new System.Drawing.Size(185, 22);
+            this.txtSatisQiymeti.TabIndex = 5;
+            // 
+            // txtAlisQiymeti
+            // 
+            this.txtAlisQiymeti.Location = new System.Drawing.Point(135, 172);
+            this.txtAlisQiymeti.Name = "txtAlisQiymeti";
+            this.txtAlisQiymeti.Size = new System.Drawing.Size(185, 22);
+            this.txtAlisQiymeti.TabIndex = 4;
+            // 
+            // txtBarkod
+            // 
+            this.txtBarkod.Location = new System.Drawing.Point(135, 67);
+            this.txtBarkod.Name = "txtBarkod";
+            this.txtBarkod.Size = new System.Drawing.Size(185, 22);
+            this.txtBarkod.TabIndex = 2;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(135, 32);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(185, 22);
+            this.txtAd.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 245);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 16);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Minimum Stok:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 16);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Satış Qiyməti:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Alış Qiyməti:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Vahid:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kateqoria:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Barkod:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Məhsul Adı:";
+            // 
+            // frmProducts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 603);
+            this.Controls.Add(this.gbProductDetails);
+            this.Controls.Add(this.dgvProducts);
+            this.MinimumSize = new System.Drawing.Size(900, 500);
+            this.Name = "frmProducts";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Məhsul İdarəçiliyi";
+            this.Load += new System.EventHandler(this.frmProducts_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.gbProductDetails.ResumeLayout(false);
+            this.gbProductDetails.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.GroupBox gbProductDetails;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ComboBox cmbVahid;
+        private System.Windows.Forms.ComboBox cmbKateqoriya;
+        private System.Windows.Forms.TextBox txtMinimumStok;
+        private System.Windows.Forms.TextBox txtSatisQiymeti;
+        private System.Windows.Forms.TextBox txtAlisQiymeti;
+        private System.Windows.Forms.TextBox txtBarkod;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ AzAgroPOS.PL/frmProducts.cs
@@ -0,0 +1,94 @@
+﻿// Fayl: AzAgroPOS.PL/frmProducts.cs
+using AzAgroPOS.BLL;
+using AzAgroPOS.Entities;
+using System;
+using System.Windows.Forms;
+
+namespace AzAgroPOS.PL
+{
+    public partial class frmProducts : Form
+    {
+        // BLL siniflərini yaradırıq
+        private readonly MehsulBLL _mehsulBll = new MehsulBLL();
+        private readonly KateqoriyaBLL _kateqoriyaBll = new KateqoriyaBLL();
+        private readonly VahidBLL _vahidBll = new VahidBLL();
+
+        public frmProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void frmProducts_Load(object sender, EventArgs e)
+        {
+            // Forma yüklənərkən lazımi məlumatları gətiririk
+            LoadCategories();
+            LoadUnits();
+            LoadProducts();
+        }
+
+        private void LoadCategories()
+        {
+            cmbKateqoriya.DataSource = _kateqoriyaBll.GetAll();
+            cmbKateqoriya.DisplayMember = "Ad"; // İstifadəçiyə görünən dəyər
+            cmbKateqoriya.ValueMember = "Id";   // Arxa planda saxlanan dəyər
+        }
+
+        private void LoadUnits()
+        {
+            cmbVahid.DataSource = _vahidBll.GetAll();
+            cmbVahid.DisplayMember = "Ad";
+            cmbVahid.ValueMember = "Id";
+        }
+
+        private void LoadProducts()
+        {
+            dgvProducts.DataSource = _mehsulBll.GetAll();
+            // Cədvəlin görünüşünü səliqəyə salmaq
+            if (dgvProducts.Columns["Id"] != null) dgvProducts.Columns["Id"].Visible = false;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Yeni məhsul obyekti yaradırıq və formadakı məlumatlarla doldururuq
+            var mehsul = new Mehsul
+            {
+                Ad = txtAd.Text,
+                Barkod = txtBarkod.Text,
+                KateqoriyaId = (int)cmbKateqoriya.SelectedValue,
+                VahidId = (int)cmbVahid.SelectedValue,
+                AlisQiymeti = decimal.Parse(txtAlisQiymeti.Text), // Xəta yoxlaması əlavə edilməlidir
+                SatisQiymeti = decimal.Parse(txtSatisQiymeti.Text), // Xəta yoxlaması əlavə edilməlidir
+                MinimumStok = int.Parse(txtMinimumStok.Text),   // Xəta yoxlaması əlavə edilməlidir
+                Aktivdir = true // Məsələn
+            };
+
+            bool result = _mehsulBll.Add(mehsul, out string message);
+
+            MessageBox.Show(message);
+
+            if (result)
+            {
+                LoadProducts(); // Cədvəli yeniləyirik
+                ClearForm();    // Formanı təmizləyirik
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+        private void ClearForm()
+        {
+            txtAd.Clear();
+            txtBarkod.Clear();
+            txtAlisQiymeti.Text = "0";
+            txtSatisQiymeti.Text = "0";
+            txtMinimumStok.Text = "0";
+            cmbKateqoriya.SelectedIndex = 0;
+            cmbVahid.SelectedIndex = 0;
+        }
+
+        // dgvProducts_CellClick, btnUpdate_Click və btnDelete_Click hadisələri növbəti addımda yazılacaq.
+    }
+}
