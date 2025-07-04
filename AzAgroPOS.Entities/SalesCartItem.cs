8632ed0ff8790e1486b8eff2c50590361afd8536
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-04 12:11:17 +0400

    kodu səliqəyə salmaq üçün ayri bir class yaratdim

---
+++ AzAgroPOS.Entities/SalesCartItem.cs
@@ -0,0 +1,12 @@
+﻿// Yeni Fayl: AzAgroPOS.Entities/SalesCartItem.cs
+namespace AzAgroPOS.Entities
+{
+    public class SalesCartItem
+    {
+        public int ProductId { get; set; }
+        public string Ad { get; set; }
+        public decimal Miqdar { get; set; }
+        public decimal VahidQiymet { get; set; }
+        public decimal YekunMebleg => Miqdar * VahidQiymet;
+    }
+}
