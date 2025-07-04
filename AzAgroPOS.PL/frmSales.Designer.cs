31c59aa812a227803a9a42d025ebffc2a6125f90
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-04 12:12:48 +0400

    satiş üçün ilkin kodlari yazdım, barkod axtarma funksiyası

---
+++ AzAgroPOS.PL/frmSales.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace AzAgroPOS.PL
+{
+    partial class frmSales
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBarcodeSearch = new System.Windows.Forms.TextBox();
+            this.dgvSalesCart = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCompleteSale = new System.Windows.Forms.Button();
+            this.lblTotalPrice = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSalesCart)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Məhsulun Barkodunu daxil et:";
+            // 
+            // txtBarcodeSearch
+            // 
+            this.txtBarcodeSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBarcodeSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBarcodeSearch.Location = new System.Drawing.Point(218, 14);
+            this.txtBarcodeSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBarcodeSearch.Name = "txtBarcodeSearch";
+            this.txtBarcodeSearch.Size = new System.Drawing.Size(662, 26);
+            this.txtBarcodeSearch.TabIndex = 0;
+            this.txtBarcodeSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBarcodeSearch_KeyDown);
+            // 
+            // dgvSalesCart
+            // 
+            this.dgvSalesCart.AllowUserToAddRows = false;
+            this.dgvSalesCart.AllowUserToDeleteRows = false;
+            this.dgvSalesCart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvSalesCart.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSalesCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSalesCart.Location = new System.Drawing.Point(9, 51);
+            this.dgvSalesCart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvSalesCart.Name = "dgvSalesCart";
+            this.dgvSalesCart.RowHeadersWidth = 51;
+            this.dgvSalesCart.RowTemplate.Height = 24;
+            this.dgvSalesCart.Size = new System.Drawing.Size(870, 396);
+            this.dgvSalesCart.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.btnCompleteSale);
+            this.panel1.Controls.Add(this.lblTotalPrice);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(9, 453);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(870, 109);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnCompleteSale
+            // 
+            this.btnCompleteSale.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCompleteSale.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompleteSale.Location = new System.Drawing.Point(692, 18);
+            this.btnCompleteSale.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCompleteSale.Name = "btnCompleteSale";
+            this.btnCompleteSale.Size = new System.Drawing.Size(163, 74);
+            this.btnCompleteSale.TabIndex = 2;
+            this.btnCompleteSale.Text = "Satışı Tamamla";
+            this.btnCompleteSale.UseVisualStyleBackColor = true;
+            this.btnCompleteSale.Click += new System.EventHandler(this.btnCompleteSale_Click);
+            // 
+            // lblTotalPrice
+            // 
+            this.lblTotalPrice.AutoSize = true;
+            this.lblTotalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalPrice.ForeColor = System.Drawing.Color.ForestGreen;
+            this.lblTotalPrice.Location = new System.Drawing.Point(264, 36);
+            this.lblTotalPrice.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotalPrice.Name = "lblTotalPrice";
+            this.lblTotalPrice.Size = new System.Drawing.Size(120, 37);
+            this.lblTotalPrice.TabIndex = 1;
+            this.lblTotalPrice.Text = "0.00 ₼";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 36);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(229, 37);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Yekun Məbləğ:";
+            // 
+            // frmSales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 571);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgvSalesCart);
+            this.Controls.Add(this.txtBarcodeSearch);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmSales";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Satış Əməliyyatı";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmSales_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSalesCart)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBarcodeSearch;
+        private System.Windows.Forms.DataGridView dgvSalesCart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTotalPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCompleteSale;
+    }
+}
---
+++ AzAgroPOS.PL/frmSales.cs
@@ -0,0 +1,114 @@
+﻿// Fayl: AzAgroPOS.PL/frmSales.cs
+using AzAgroPOS.BLL;
+using AzAgroPOS.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AzAgroPOS.PL
+{
+       public partial class frmSales : Form
+    {
+        private readonly MehsulBLL _mehsulBll = new MehsulBLL();
+        // Səbətdəki məhsulları yadda saxlamaq üçün List
+        private BindingList<SalesCartItem> _cartItems = new BindingList<SalesCartItem>();
+
+        public frmSales()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSales_Load(object sender, EventArgs e)
+        {
+            // Cədvəli (DataGridView) səbətimizə bağlayırıq
+            dgvSalesCart.DataSource = _cartItems;
+            SetupDataGrid();
+        }
+
+        private void SetupDataGrid()
+        {
+            // Sütunların adlarını və görünüşünü dəyişirik
+            dgvSalesCart.Columns["ProductId"].Visible = false;
+            dgvSalesCart.Columns["Ad"].HeaderText = "Məhsul Adı";
+            dgvSalesCart.Columns["Miqdar"].HeaderText = "Miqdar";
+            dgvSalesCart.Columns["VahidQiymet"].HeaderText = "Vahid Qiyməti";
+            dgvSalesCart.Columns["YekunMebleg"].HeaderText = "Yekun Məbləğ";
+        }
+
+        private void txtBarcodeSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Yalnız Enter düyməsinə basıldıqda işləsin
+            if (e.KeyCode == Keys.Enter)
+            {
+                string barcode = txtBarcodeSearch.Text.Trim();
+                if (string.IsNullOrEmpty(barcode)) return;
+
+                // Barkoda görə məhsulu axtarırıq
+                var product = _mehsulBll.GetByBarcode(barcode);
+
+                if (product != null)
+                {
+                    // Məhsul tapılıbsa, səbətə əlavə edirik
+                    AddToCart(product);
+                }
+                else
+                {
+                    MessageBox.Show("Bu barkoda uyğun məhsul tapılmadı.", "Məlumat", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+                // Axtarış xanasını təmizləyirik və fokusu qaytarırıq
+                txtBarcodeSearch.Clear();
+                txtBarcodeSearch.Focus();
+            }
+        }
+
+        private void AddToCart(Mehsul product)
+        {
+            // Məhsulun səbətdə olub-olmadığını yoxlayırıq
+            var existingItem = _cartItems.FirstOrDefault(item => item.ProductId == product.Id);
+
+            if (existingItem != null)
+            {
+                // Əgər varsa, sayını bir artırırıq
+                existingItem.Miqdar++;
+            }
+            else
+            {
+                // Yoxdursa, yeni bir sətir olaraq əlavə edirik
+                _cartItems.Add(new SalesCartItem
+                {
+                    ProductId = product.Id,
+                    Ad = product.Ad,
+                    Miqdar = 1,
+                    VahidQiymet = product.SatisQiymeti
+                });
+            }
+
+            // Cədvəli və yekun məbləği yeniləyirik
+            RefreshCartDisplay();
+        }
+
+        private void RefreshCartDisplay()
+        {
+            dgvSalesCart.Refresh();
+            UpdateTotals();
+        }
+
+        private void UpdateTotals()
+        {
+            decimal total = _cartItems.Sum(item => item.YekunMebleg);
+            lblTotalPrice.Text = total.ToString("F2") + " ₼";
+        }
+
+        private void btnCompleteSale_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
