1743721165b313468c081ae78794c7ea6803673c
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-07-06 18:53:31 +0400

    feat(dashboard): Ana səhifəyə satışların vizual qrafiki əlavə edildi
    Dashboard-a son 7 günün satışlarını göstərən sütunlu qrafik (Column Chart) əlavə edildi. Qrafiki doldurmaq üçün yeni backend məntiqi yaradıldı və Chart nəzarət elementi tema rənglərinə uyğunlaşdırıldı.

--- AzAgroPOS.DAL/DashboardDAL.cs
+++ AzAgroPOS.DAL/DashboardDAL.cs
@@ -1,6 +1,7 @@
 ﻿using AzAgroPOS.DAL.Helpers;
 using AzAgroPOS.Entities;
 using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace AzAgroPOS.DAL
@@ -39,6 +40,42 @@ namespace AzAgroPOS.DAL
                 catch (Exception ex) { throw; }
             }
             return stats;
+        }
+        // DashboardDAL.cs sinifinin içinə əlavə edin
+
+        /// <summary>
+        /// Son 7 günün satış məbləğlərini tarixə görə qruplaşdıraraq qaytarır.
+        /// </summary>
+        public Dictionary<DateTime, decimal> GetSalesForLast7Days()
+        {
+            var dailySales = new Dictionary<DateTime, decimal>();
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                // SQL Server-in GETDATE() funksiyası ilə son 7 günü dinamik olaraq hesablayırıq
+                var query = @"
+            SELECT
+                CAST(satis_tarixi AS DATE) as SatisGunu,
+                SUM(yekun_mebleg) as CemiMebleg
+            FROM satislar
+            WHERE satis_tarixi >= DATEADD(day, -7, GETDATE())
+            GROUP BY CAST(satis_tarixi AS DATE)
+            ORDER BY SatisGunu;";
+
+                var command = new SqlCommand(query, conn);
+                try
+                {
+                    conn.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            dailySales.Add((DateTime)reader["SatisGunu"], (decimal)reader["CemiMebleg"]);
+                        }
+                    }
+                }
+                catch (Exception ex) { throw; }
+            }
+            return dailySales;
         }
     }
 }
