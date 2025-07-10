f9c677df4843bfdb4d52e843248edc2412065d10
Təhməz Muradoff <muradofftehmez01@gmail.com>
2025-07-10 14:56:50 +0400

    Builds out the core components for user authentication.
    - BLL: Creates `AuthService` to handle login business logic, including validation, password verification using SHA256, and user status checks.
    - DAL: Adds `IstifadeciRepository` with a `GetByEmail` method, using parameterized queries to prevent SQL injection.
    - PL: Implements the `LoginForm` UI with fields for email and password. The login button is now wired to the `AuthService` to validate credentials.
    
    This commit establishes the full vertical slice for the login feature, from the UI to the database."

---
+++ AzAgroPOS.PL/LoginForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace AzAgroPOS.PL
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 85);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifrə";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Location = new System.Drawing.Point(115, 117);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(208, 39);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Daxil Ol";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(118, 39);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(205, 29);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(118, 78);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(205, 29);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 202);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistemə Giriş";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
---
+++ AzAgroPOS.PL/LoginForm.cs
@@ -0,0 +1,41 @@
+﻿using AzAgroPOS.BLL.Services;
+using System;
+using System.Windows.Forms;
+
+namespace AzAgroPOS.PL
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                // BLL-dəki AuthService-dən bir nümunə yaradırıq
+                var authService = new AuthService();
+
+                // TextBox-lardan dəyərləri götürürük
+                string email = txtEmail.Text;
+                string password = txtPassword.Text;
+
+                // Login metodunu çağırıb nəticəni alırıq
+                string netice = authService.Login(email, password);
+
+                // Nəticəni istifadəçiyə göstəririk
+                MessageBox.Show(netice, "Məlumat", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Əgər giriş uğurludursa, bu pəncərəni bağlayıb ana pəncərəni aça bilərik
+                // Bu hissəni gələcəkdə əlavə edəcəyik.
+            }
+            catch (Exception ex)
+            {
+                // Gözlənilməyən bir xəta baş verərsə
+                MessageBox.Show($"Xəta baş verdi: {ex.Message}", "Xəta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
