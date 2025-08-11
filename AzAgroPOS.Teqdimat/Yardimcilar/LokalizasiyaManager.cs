49bf59bc1ae8b7c0adadf48a49f648ad3fe02b1c
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-08-11 23:51:50 +0400

    dil ayarlari yükləndi

---
+++ AzAgroPOS.Teqdimat/Yardimcilar/LokalizasiyaManager.cs
@@ -0,0 +1,28 @@
+﻿// Fayl: AzAgroPOS.Teqdimat/Yardimcilar/LokalizasiyaManager.cs
+namespace AzAgroPOS.Teqdimat.Yardimcilar;
+
+using System.Globalization;
+using System.Threading;
+
+public static class LokalizasiyaManager
+{
+    /// <summary>
+    /// Proqramın cari mədəniyyətini (dilini) dəyişir.
+    /// </summary>
+    /// <param name="cultureCode">Dəyişmək istədiyiniz dilin kodu (məs: "az-AZ" və ya "en-US").</param>
+    public static void SetCulture(string cultureCode)
+    {
+        try
+        {
+            CultureInfo cultureInfo = new CultureInfo(cultureCode);
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+        }
+        catch (CultureNotFoundException)
+        {
+            CultureInfo defaultCulture = new CultureInfo("az-AZ");
+            Thread.CurrentThread.CurrentCulture = defaultCulture;
+            Thread.CurrentThread.CurrentUICulture = defaultCulture;
+        }
+    }
+}
