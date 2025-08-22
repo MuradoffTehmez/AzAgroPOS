3bef7c8f743e43e55195cff8633912dd86704c0c
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-08-22 14:52:13 +0400

    Nisyə  modeli üçün sənədləşdirmə əlavə olundu

--- AzAgroPOS.Mentiq/DTOs/NisyeHereketiDto.cs
+++ AzAgroPOS.Mentiq/DTOs/NisyeHereketiDto.cs
@@ -2,11 +2,25 @@
 namespace AzAgroPOS.Mentiq.DTOs;
 
 using AzAgroPOS.Varliglar;
-
+/// <summary>
+/// bu class, nisye hərəkətlərini təmsil edir.
+/// </summary>
 public class NisyeHereketiDto
 {
+    /// <summary>
+    /// Tarix, hərəkətin baş verdiyi tarixi saxlayır.
+    /// </summary>
     public DateTime Tarix { get; set; }
+    /// <summary>
+    /// EmeliyyatNovu, hərəkətin növünü (məsələn, borc, ödəniş və s.) saxlayır.
+    /// </summary>
     public string EmeliyyatNovu { get; set; } = string.Empty;
+    /// <summary>
+    /// Mebleg, hərəkət üçün ödənilən və ya alınan məbləği saxlayır.
+    /// </summary>
     public decimal Mebleg { get; set; }
+    /// <summary>
+    /// qeyd, hərəkət haqqında əlavə məlumat və ya qeydləri saxlayır.
+    /// </summary>
     public string Qeyd { get; set; } = string.Empty;
 }
