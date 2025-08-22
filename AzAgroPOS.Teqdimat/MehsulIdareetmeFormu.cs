a82c89d626e1354b01b98f0c771e09fc00e232c2
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-08-22 20:03:16 +0400

    SƏNƏDLƏŞDİRMƏ EDİLDİ

--- AzAgroPOS.Teqdimat/MehsulIdareetmeFormu.cs
+++ AzAgroPOS.Teqdimat/MehsulIdareetmeFormu.cs
@@ -6,10 +6,19 @@ using AzAgroPOS.Teqdimat.Interfeysler;
 using AzAgroPOS.Teqdimat.Teqdimatcilar;
 using System.Data;
 
+/// <summary>
+/// B
+/// </summary>
 public partial class MehsulIdareetmeFormu : BazaForm, IMehsulIdareetmeView
 {
+    /// <summary>
+    /// 
+    /// </summary>
     private MehsulPresenter _presenter;
 
+    /// <summary>
+    ///  
+    /// </summary>
     public MehsulIdareetmeFormu()
     {
         InitializeComponent();
@@ -18,22 +27,68 @@ public partial class MehsulIdareetmeFormu : BazaForm, IMehsulIdareetmeView
 
     // IMehsulIdareetmeView interfeysinin implementasiyası
     #region View Implementasiyası
+    /// <summary>
+    /// 
+    /// </summary>
     public string MehsulId { get => txtId.Text; set => txtId.Text = value; }
+    /// <summary>
+    /// 
+    /// </summary>
     public string MehsulAdi { get => txtAd.Text; set => txtAd.Text = value; }
+    /// <summary>
+    /// 
+    /// </summary>
     public string StokKodu { get => txtStokKodu.Text; set => txtStokKodu.Text = value; }
+    /// <summary>
+    /// 
+    /// </summary>
     public string Barkod { get => txtBarkod.Text; set => txtBarkod.Text = value; }
+    /// <summary>
+    /// 
+    /// </summary>
     public string SatisQiymeti { get => txtSatisQiymeti.Text; set => txtSatisQiymeti.Text = value; }
+    /// <summary>
+    /// 
+    /// </summary>
     public string MovcudSay { get => txtMevcudSay.Text; set => txtMevcudSay.Text = value; }
+    /// <summary>
+    /// 
+    /// </summary>
     public string AxtarisMetni { get => txtAxtar.Text; set => txtAxtar.Text = value; }
 
+    /// <summary>
+    /// 
+    /// </summary>
     public event EventHandler FormYuklendi_Istek;
+    /// <summary>
+    /// 
+    /// </summary>
     public event EventHandler MehsulElaveEt_Istek;
+    /// <summary>
+    /// 
+    /// </summary>
     public event EventHandler MehsulYenile_Istek;
+    /// <summary>
+    /// 
+    /// </summary>
     public event EventHandler MehsulSil_Istek;
+    /// <summary>
+    /// 
+    /// </summary>
     public event EventHandler FormuTemizle_Istek;
+    /// <summary>
+    /// 
+    /// </summary>
     public event EventHandler CedvelSecimiDeyisdi_Istek;
+    /// <summary>
+    /// 
+    /// </summary>
     public event EventHandler Axtaris_Istek;
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="mehsullar"></param>
     public void MehsullariGoster(IEnumerable<MehsulDto> mehsullar)
     {
         // Mümkün null vəziyyətlərinə qarşı DataSource-u təhlükəsiz şəkildə təyin edirik.
@@ -56,6 +111,14 @@ public partial class MehsulIdareetmeFormu : BazaForm, IMehsulIdareetmeView
         }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="mesaj"></param>
+    /// <param name="basliq"></param>
+    /// <param name="düymələr"></param>
+    /// <param name="ikon"></param>
+    /// <returns></returns>
     public DialogResult MesajGoster(string mesaj, string basliq, MessageBoxButtons düymələr, MessageBoxIcon ikon)
     {
         return MessageBox.Show(mesaj, basliq, düymələr, ikon);
@@ -64,31 +127,61 @@ public partial class MehsulIdareetmeFormu : BazaForm, IMehsulIdareetmeView
 
     // Form hadisələrini Presenter-ə ötürmək
     #region Hadisə Ötürücüləri
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void MehsulIdareetmeFormu_Load(object sender, EventArgs e)
     {
         FormYuklendi_Istek?.Invoke(this, EventArgs.Empty);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void btnElaveEt_Click(object sender, EventArgs e)
     {
         MehsulElaveEt_Istek?.Invoke(this, EventArgs.Empty);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void btnYenile_Click(object sender, EventArgs e)
     {
         MehsulYenile_Istek?.Invoke(this, EventArgs.Empty);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void btnSil_Click(object sender, EventArgs e)
     {
         MehsulSil_Istek?.Invoke(this, EventArgs.Empty);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void btnTemizle_Click(object sender, EventArgs e)
     {
         FormuTemizle_Istek?.Invoke(this, EventArgs.Empty);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void dgvMehsullar_SelectionChanged(object sender, EventArgs e)
     {
         if (dgvMehsullar.CurrentRow != null && dgvMehsullar.CurrentRow.DataBoundItem is MehsulDto secilmisMehsul)
@@ -98,6 +191,11 @@ public partial class MehsulIdareetmeFormu : BazaForm, IMehsulIdareetmeView
         }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void txtAxtar_TextChanged(object sender, EventArgs e)
     {
         Axtaris_Istek?.Invoke(this, EventArgs.Empty);
