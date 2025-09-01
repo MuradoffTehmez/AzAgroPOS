95ad3b658f1f1be46859df99d6f449146de931c3
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-09-02 00:24:58 +0400

    miqresion

---
+++ AzAgroPOS.Verilenler/Migrations/20250901201238_MehsulXanaAdlariYekunStandartlasdirma.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AzAgroPOS.Verilenler.Migrations
+{
+    /// <inheritdoc />
+    public partial class MehsulXanaAdlariYekunStandartlasdirma : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Miqdar",
+                table: "SatisDetallari",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "UmumiMebleg",
+                table: "SatisDetallari",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StokKodu",
+                table: "Mehsullar",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Barkod",
+                table: "Mehsullar",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Aktivdir",
+                table: "Mehsullar",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OlcuVahidiId",
+                table: "Mehsullar",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Mehsullar",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Aktivdir", "OlcuVahidiId" },
+                values: new object[] { true, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Mehsullar",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Aktivdir", "OlcuVahidiId" },
+                values: new object[] { true, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Mehsullar",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Aktivdir", "OlcuVahidiId" },
+                values: new object[] { true, 0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UmumiMebleg",
+                table: "SatisDetallari");
+
+            migrationBuilder.DropColumn(
+                name: "Aktivdir",
+                table: "Mehsullar");
+
+            migrationBuilder.DropColumn(
+                name: "OlcuVahidiId",
+                table: "Mehsullar");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Miqdar",
+                table: "SatisDetallari",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StokKodu",
+                table: "Mehsullar",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Barkod",
+                table: "Mehsullar",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
