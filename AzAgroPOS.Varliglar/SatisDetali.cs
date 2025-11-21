65bdcd161e1c3b43e08c023fcd7195d87fcb3f28
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-11-21 11:44:53 +0400

    ex

--- AzAgroPOS.Varliglar/SatisDetali.cs
+++ AzAgroPOS.Varliglar/SatisDetali.cs
@@ -4,9 +4,9 @@ namespace AzAgroPOS.Varliglar
     public class SatisDetali : BazaVarligi
     {
         public int SatisId { get; set; }
-        public Satis Satis { get; set; }
+        public Satis Satis { get; set; } = null!;
         public int MehsulId { get; set; }
-        public Mehsul Mehsul { get; set; }
+        public Mehsul Mehsul { get; set; } = null!;
         public decimal Miqdar { get; set; }
         public decimal Qiymet { get; set; }
         public decimal UmumiMebleg { get; set; }
--- AzAgroPOS.Varliglar/TemirMerhelesi.cs
+++ AzAgroPOS.Varliglar/TemirMerhelesi.cs
@@ -3,9 +3,9 @@
     public class TemirMerhelesi : BazaVarligi
     {
         public int TemirId { get; set; }
-        public virtual Temir Temir { get; set; }
+        public virtual Temir Temir { get; set; } = null!;
         public DateTime Tarix { get; set; }
-        public string Aciqlama { get; set; }
+        public string Aciqlama { get; set; } = string.Empty;
         public TemirStatusu Status { get; set; }
     }
 }
